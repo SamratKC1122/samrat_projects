988454cc5439331739a633e87d9034b304397020
SamratKC1122 <czc2p7pk8b@privaterelay.appleid.com>
2026-01-13 23:04:50 +0545

    Implement draggable about window functionality

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const windowEl = document.getElementById("aboutWindow");
+const titleBar = windowEl.querySelector(".title-bar");
+
+function openWindow() {
+  windowEl.style.display = "block";
+}
+
+function closeWindow() {
+  windowEl.style.display = "none";
+}
+
+let isDragging = false;
+let offsetX, offsetY;
+
+titleBar.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  offsetX = e.clientX - windowEl.offsetLeft;
+  offsetY = e.clientY - windowEl.offsetTop;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+  windowEl.style.left = e.clientX - offsetX + "px";
+  windowEl.style.top = e.clientY - offsetY + "px";
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+});
 
